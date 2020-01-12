c5502306dff6964b5bd347d10a7a6d63c61d7450
Erwin Lejeune <erwin.lejeune15@gmail.com>
2020-01-12 17:22:34 +0100

    ✨ add API library

---
+++ lib/mcp.cpp
@@ -0,0 +1,106 @@
+#include "mcp.h"
+
+mcp23s17::mcp23s17() {}
+
+void mcp23s17::writeReg(uint8_t reg , uint8_t val) {
+    beginTransaction();
+    transfer8(0x40);
+    transfer8(reg);
+    transfer8(val);
+    endTransaction();
+}
+
+uint8_t mcp23s17::readReg(uint8_t reg) {
+    beginTransaction();
+    transfer8(0x41);
+    transfer8(reg);
+    uint8_t val = transfer8(0);
+    endTransaction();
+    return val;
+}
+
+void mcp23s17::setBitInReg(uint8_t reg, uint8_t bitNum) {
+    uint8_t val = readReg(reg);
+    val |= 1 << bitNum;
+    writeReg(reg, val);
+}
+
+void mcp23s17::clearBitInReg(uint8_t reg , uint8_t bitNum) {
+    uint8_t val = readReg(reg);
+    val &= ~(1 << bitNum);
+    writeReg(reg, val);
+}
+
+void mcp23s17::begin() {
+    setupSPI();
+}
+
+int mcp23s17::pinMode(port p, unsigned char bitNum, mode m) {
+    if (p == PORTA) {
+        switch(m) {
+            case INPUT :
+                setBitInReg(IODIRA, bitNum);
+                break;
+            case OUTPUT :
+                clearBitInReg(IODIRA, bitNum);
+                break;
+            case INPUT_PULLUP :
+                setBitInReg(IODIRA, bitNum);
+                setBitInReg(GPPUA, bitNum);
+                break;
+        }
+    } else if (p == PORTB) {
+        switch(m) {
+            case INPUT :
+                setBitInReg(IODIRB, bitNum);
+                break;
+            case OUTPUT :
+                clearBitInReg(IODIRB, bitNum);
+                break;
+            case INPUT_PULLUP :
+                setBitInReg(IODIRB, bitNum);
+                setBitInReg(GPPUB, bitNum);
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void mcp23s17::digitalWrite(port p, unsigned char bitNum, bool value) {
+    if (p == PORTA) {
+        if (value == 0) {
+            clearBitInReg(MCP_GPIOA, bitNum);
+        } else {
+            setBitInReg(MCP_GPIOA, bitNum);
+        }
+    }
+    else if (p == PORTB) {
+        if(value == 0) {
+            clearBitInReg(MCP_GPIOB, bitNum);
+        } else {
+            setBitInReg(MCP_GPIOB, bitNum);
+        }
+    }
+}
+
+uint8_t mcp23s17::readBits(port p) {
+    uint8_t val;
+    if (p == PORTA) {
+        val = readReg(MCP_GPIOA);
+    } else if (p == PORTB) {
+        val = readReg(MCP_GPIOB);
+    }
+    return val;
+}
+
+// WIP
+void attachInterrupt(port p, uint8_t bitNum, itType type, mcpCallback callback) {
+    setBitInReg(GPINTENB, bitNum);
+    setBitInReg(DEFVALB, bitNum);
+    setBitInReg(INTCONB, bitNum);
+
+    mcpCallback interruptTable[8];
+
+    interruptTable[bitNum] = callback;
+}
---
+++ lib/mcp.h
@@ -0,0 +1,57 @@
+#ifndef mcph
+#define mcph
+
+#define IODIRA 0x0
+#define IODIRB 0x1
+#define IPOLA  0x2
+#define IPOLB  0x3
+#define GPINTENA 0x4
+#define GPINTENB 0x5
+#define DEFVALA 0x6
+#define DEFVALB 0x7
+#define INTCONA 0x8
+#define INTCONB 0x9
+#define IOCONA 0xA
+#define IOCONB 0xB
+#define GPPUA 0xC
+#define GPPUB 0xD
+#define INTFA 0xE
+#define INTFB 0xF
+#define INTCAPA 0x10
+#define INTCAPB 0x11
+#define MCP_GPIOA 0x12
+#define MCP_GPIOB 0x13
+#define OLATA 0x14
+#define OLATB 0x15
+
+#include <stdint.h>
+#include "spi.h"
+
+typedef void (*mcpCallBack)();
+
+class mcp23s17
+{
+    public:
+    mcp23s17();
+
+    enum port { PORTA, PORTB};
+    enum mode { OUTPUT, INPUT, INPUT_PULLUP };
+    enum itType { RISING , FALLING , BOTH };
+
+    void writeReg(uint8_t reg , uint8_t val);
+    uint8_t readReg (uint8_t reg);
+
+    void setBitInReg ( uint8_t reg , uint8_t bitNum );
+    void clearBitInReg ( uint8_t reg , uint8_t bitNum );
+
+    void begin();
+    int pinMode(port p, unsigned char bitNum, mode m);
+    void digitalWrite(port p, unsigned char bitNum, bool value);
+    uint8_t readBits(port p);
+
+    void attachInterrupt(port p, uint8_t bitNum, itType type, mcpCallback callback);
+};
+
+extern mcp23s17 ioExt;
+
+#endif
